2977aa25f954e7dbca36b040e25c7e527fc5d87d
Ibnu Sina Sudrajat <ibnusinasudrajat@gmail.com>
2026-07-28 11:07:19 +0700

    fix: import shared_preferences in InfinityBypassEngine.dart

--- lib/services/InfinityBypassEngine.dart
+++ lib/services/InfinityBypassEngine.dart
@@ -23,7 +23,6 @@ import 'dart:async';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import '../utils/app_config.dart';
 import '../utils/app_constants.dart';
 import 'infinity_bypass_exception.dart';
 
